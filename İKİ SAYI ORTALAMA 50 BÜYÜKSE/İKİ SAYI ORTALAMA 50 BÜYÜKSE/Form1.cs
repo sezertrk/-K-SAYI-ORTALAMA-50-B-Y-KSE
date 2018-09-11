c8739694a04ed6a372655b9a45986924aebdf3e1
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-11 14:25:05 +0300

    Add files via upload

---
+++ İKİ SAYI ORTALAMA 50 BÜYÜKSE/İKİ SAYI ORTALAMA 50 BÜYÜKSE/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace İKİ_SAYI_ORTALAMA_50_BÜYÜKSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox1.Text);
+            int s2 = int.Parse(textBox2.Text);
+            int ort = 0;
+            ort = (s1 + s2) / 2;
+            if (ort < 50)
+            {
+                textBox3.Text = "Zarar";
+            }
+            else
+            {
+                textBox3 .Text = "Kar";
+            }
+        }
+    }
+}
---
+++ İKİ SAYI ORTALAMA 50 BÜYÜKSE/İKİ SAYI ORTALAMA 50 BÜYÜKSE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace İKİ_SAYI_ORTALAMA_50_BÜYÜKSE
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
